e9e4b7d0bbb54f4ac395b887185ff2373e427225
dudamay <maysaeduarda1006@gmail.com>
2024-05-23 11:14:55 -0400

    add explode function

--- js/classes/Bomb.js
+++ js/classes/Bomb.js
@@ -1,25 +1,60 @@
 class Bomb {
-static radius = 30;
-constructor({position, velocity}) {
-    this.position = position;
-    this.velocity = velocity;
-    this.radius = 0;
-    this.color = " ";
-    this.opacity = 1;
-    this.active = false;
+    static radius = 30;
+    constructor({position, velocity}) {
+        this.position = position;
+        this.velocity = velocity;
+        this.radius = 0;
+        this.color = " ";
+        this.opacity = 1;
+        this.active = false;
 
-    gsap.to(this, {
-        radius:30
-    });
- }
- draw(){
-    c.save();
-    c.globalAlpha = this.opacity;
-    c.beginPath();
-    c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2);
-    c.closePath();
-    c.fillStyle = this.color;
-    c.fill();
-    c.restore();
+        gsap.to(this, {
+            radius: 30
+        });
+    }""
+
+    draw() {
+        c.save();
+        c.globalAlpha = this.opacity;
+        c.beginPath();
+        c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2);
+        c.closePath();
+        c.fillStyle = this.color;
+        c.fill();
+        c.restore();
+    }
+    update() {
+        this.position.x += this.velocity.x;
+        this.position.y += this.velocity.y;
+        this.draw();
+
+        if( 
+            this.position.x + this.radius + this.velocity.x >= Canvas.width ||
+            this.position.x - this.radius + this.velocity.x <= 0
+        ) {
+            this.velocity.x = -this.velocity.x;
+        } else if (
+            this.position.y + this.radius + this.velocity.y  >= Canvas.height ||
+            this.position.y - this.radius + this.velocity.y <= 0
+        )
+        this.velocity.y = -this.velocity.y
+    }
+    
+    explode() {
+        audio.bomb.play();
+        this.active = true;
+        this.velocity.x = 0;
+        this.velocity.y = 0;
+
+        gsap.to(this, {
+            radius: 200,
+            color: "red"
+        });
+
+        gsap.to(this,{
+            delay: 0.1,
+            Opacity:0,
+            duration:0.15
+        });
  }
 }
