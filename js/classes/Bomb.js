c25fa0876036c7d61270435f10225a13ffbeff68
dudamay <maysaeduarda1006@gmail.com>
2024-05-23 10:27:03 -0400

    add draw function

--- js/classes/Bomb.js
+++ js/classes/Bomb.js
@@ -11,7 +11,15 @@ constructor({position, velocity}) {
     gsap.to(this, {
         radius:30
     });
-
-    
+ }
+ draw(){
+    c.save();
+    c.globalAlpha = this.opacity;
+    c.beginPath();
+    c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2);
+    c.closePath();
+    c.fillStyle = this.color;
+    c.fill();
+    c.restore();
  }
 }
